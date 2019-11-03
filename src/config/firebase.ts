791acc934389215426224734d59e2e7b1b67b08b
HiroakiSasaki <skth.s.0410@gmail.com>
2019-11-03 16:25:19 +0900

    firebaseの環境変数を定義

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth'
+
+const config = {
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: "ts-react-portfolio.firebaseapp.com",
+    databaseURL: "https://ts-react-portfolio.firebaseio.com",
+    projectId: "ts-react-portfolio",
+    storageBucket: "ts-react-portfolio.appspot.com",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+firebase.initializeApp(config)
+
+export default firebase
